635c3ef77d2ea10fc6a50e6b1418c102dddd2606
gabypachano <gabrielapachano.13@gmail.com>
2023-05-22 00:03:43 -0300

    creacion de base de datos y modelos

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,55 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.STRING
+    },
+    height: {
+      type: DataTypes.STRING
+    },
+    weight: {
+      type: DataTypes.STRING
+    },
+  }, { freezeTableName: true, timestamps: false}
+  );
 };
+
+//! Son obligatorios
+// ID. * -> id
+// Nombre. * -> name
+// Imagen. * -> image
+// Vida. * -> hp
+// Ataque. * -> attack
+// Defensa. * -> defense
+
+// Velocidad. -> speed
+// Altura. -> height
+// Peso. -> weight
