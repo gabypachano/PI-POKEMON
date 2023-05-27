94e8e68a3dce835ea2dbde56fb2e9ff5e8276a6e
gabypachano <gabrielapachano.13@gmail.com>
2023-05-27 17:16:55 -0300

    primeras configuraciones del front

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Detail from './views/Detail/Detail';
+import Form from './views/Form/Form';
+import Landing from './views/Landing/Landing';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <>
+    <NavBar />
+    <Switch>
+      <Route path='/home'>
+        <Home />
+      </Route>
+      <Route path='/detail/:id'>
+        <Detail />
+      </Route>
+      <Route path='/form'>
+        <Form />
+      </Route>
+      <Route exact path='/'>
+        <Landing />
+      </Route>
+    </Switch>
+    </>
   );
 }
 
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const rootReducer = () => {
+  return (
+    <div>reducer</div>
+  )
+}
+
+export default rootReducer
