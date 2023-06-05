b98b26d8f567ab4c948947ac790c97f485dbcd1e
gabypachano <gabrielapachano.13@gmail.com>
2023-06-04 21:27:12 -0300

    navBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css'; // Importa el archivo CSS aquí
 import SearchBar from '../SearchBar/SearchBar';
 import Filters from '../Filters/Filters';
+import style from './NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <div className="nav-container">
-      <div className="img-container">
+    <div className={style.container}>
+      
         <Link to="/">
           <img
             src="https://cdn.vox-cdn.com/thumbor/yysokBKQ-hg7Ap68uxCteAXbY6A=/0x0:2870x1591/1400x933/filters:focal(1992x551:2450x1009):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/72110370/Screen_Shot_2023_03_24_at_9.25.38_AM.0.png"
             alt="img-landing"
           />
         </Link>
-      </div>
-      <div className="link-container">
         <Link to="/home">
           <button>Home</button>
         </Link>
         <Link to="/create">
-          <button>Create</button>
+          <button>Crear</button>
         </Link>
-      </div>
+     
       <div>
         <SearchBar />
       </div>
+      <div>
       <Filters />
-    </div>
+      </div>
+      </div>
   );
 };
 
